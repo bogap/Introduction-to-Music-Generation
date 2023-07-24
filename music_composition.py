6e80f6a7651c3d5f2c6eb5448bb9f092fcdb25e2
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2023-07-25 00:20:10 +0300

    upd1

--- music_composition.py
+++ music_composition.py
@@ -9,15 +9,12 @@ def play():
         while pygame.mixer.music.get_busy():
             continue
 
-    # Replace 'file1.mid' and 'file2.mid' with the paths to your MIDI files
     midi_file1 = 'verse_melody.mid'
     midi_file2 = 'verse.mid'
     midi_file3 = 'chorus.mid'
 
-    # Play the first MIDI file
     play_midi(midi_file1)
 
-    # Play the second MIDI file
     play_midi(midi_file2)
     play_midi(midi_file2)
 
